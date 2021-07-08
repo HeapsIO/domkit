HeapsIO/domkit

commit 542c6b9edc23c7e9d899b183b7063357d364b6dd

@:uiInitFunction: allow overriding in subclasses, support resetting to constructor with @:uiInitFunction()

domkit/Macros.hx
@@ -435,7 +435,7 @@ class Macros {
 			inits.push({ expr : initExpr.expr, pos : initExpr.pos });
 			initExpr.expr = EBlock(inits);
 		}
-		var initFunc = "new";
+		var initFunc = null;
 		var initArgs = null;
 
 		var ccur = cl;
@@ -443,11 +443,16 @@ class Macros {
 			if( ccur.meta.has(":uiInitFunction") || (initArgs == null && ccur.meta.has(":domkitInitArgs")) ) {
 				for( m in ccur.meta.get() )
 					switch( m.name ) {
-					case ":uiInitFunction" if( m.params.length == 1 ):
-						switch( m.params[0].expr ) {
-						case EConst(CIdent(name)): initFunc = name;
-						default: Context.warning("Invalid @:uiInitFunction(funName)", m.pos);
+					case ":uiInitFunction" if(initFunc == null):
+						if( m.params.length == 1 ) {
+							switch( m.params[0].expr ) {
+							case EConst(CIdent(name)): initFunc = name;
+							default: Context.warning("Invalid @:uiInitFunction(funName)", m.pos);
+							}
 						}
+						else if( m.params.length == 0 )
+							initFunc = "new";
+						
 					case ":domkitInitArgs" if( initArgs == null && ccur != cl ):
 						switch( m.params[0].expr ) {
 						case ECheckType({ expr : EConst(CIdent(name)) }, TAnonymous(fields))
@@ -462,6 +467,8 @@ class Macros {
 			ccur = ccur.superClass.t.get();
 		}
 
+		if( initFunc == null )
+			initFunc = "new";
 		var found = null;
 		for( f in fields )
 			if( f.name == initFunc ) {

commit 47e03030127735aa61e781deeed640ca6ac40462

fix initArgs broken by 542c6b9edc23c7e9d899b183b7063357d364b6dd

domkit/Macros.hx
@@ -439,11 +439,11 @@ class Macros {
 		var initArgs = null;
 
 		var ccur = cl;
-		while( true ) {
-			if( ccur.meta.has(":uiInitFunction") || (initArgs == null && ccur.meta.has(":domkitInitArgs")) ) {
-				for( m in ccur.meta.get() )
+		while( initFunc == null ) {
+			if( ccur.meta.has(":uiInitFunction") ) {
+				for( m in ccur.meta.get() ) {
 					switch( m.name ) {
-					case ":uiInitFunction" if(initFunc == null):
+					case ":uiInitFunction":
 						if( m.params.length == 1 ) {
 							switch( m.params[0].expr ) {
 							case EConst(CIdent(name)): initFunc = name;
@@ -452,8 +452,21 @@ class Macros {
 						}
 						else if( m.params.length == 0 )
 							initFunc = "new";
-						
-					case ":domkitInitArgs" if( initArgs == null && ccur != cl ):
+					}
+				}
+			}
+			if( ccur.superClass == null ) break;
+			ccur = ccur.superClass.t.get();
+		}
+		if( initFunc == null )
+			initFunc = "new";
+
+		var ccur = cl;
+		while( initArgs == null ) {
+			if( ccur.meta.has(":domkitInitArgs") ) {
+				for( m in ccur.meta.get() ) {
+					switch( m.name ) {
+					case ":domkitInitArgs" if( ccur != cl ):
 						switch( m.params[0].expr ) {
 						case ECheckType({ expr : EConst(CIdent(name)) }, TAnonymous(fields))
 						   | EParenthesis({ expr : ECheckType({ expr : EConst(CIdent(name)) }, TAnonymous(fields)) }):
@@ -462,13 +475,12 @@ class Macros {
 						}
 					default:
 					}
+				}
 			}
 			if( ccur.superClass == null ) break;
 			ccur = ccur.superClass.t.get();
 		}
 
-		if( initFunc == null )
-			initFunc = "new";
 		var found = null;
 		for( f in fields )
 			if( f.name == initFunc ) {