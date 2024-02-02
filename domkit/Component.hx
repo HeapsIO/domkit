c3dc009edfbed866422b787e227ef51c313d88b4
Nicolas Cannasse <ncannasse@gmail.com>
2024-02-02 17:49:24 +0100

    fixed fix

--- domkit/Component.hx
+++ domkit/Component.hx
@@ -100,7 +100,8 @@ class Component<BaseT,T> {
 				switch( initExpr.expr ) {
 				case EBlock(el):
 					var t = comp.baseType;
-					el.unshift(parent == null || parent.expr.match(EConst(CIdent("null"))) ? macro var tmp : domkit.Properties<$t> = null : macro var tmp = $parent.dom);
+					var tcomp = @:privateAccess Macros.componentsType;
+					el.unshift(parent == null || parent.expr.match(EConst(CIdent("null"))) ? macro var tmp : domkit.Properties<$tcomp> = null : macro var tmp = $parent.dom);
 					el.push(macro (cast tmp.obj : $t));
 				default: throw "assert";
 				}
