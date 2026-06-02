cac1106617224cd0a5116543cfbf8dc53c33a68b
Leonardo Jeanteur <leonardo@shirogames.com>
2026-06-02 17:54:38 +0200

    Use subtraction on ids for sortByPriority fallback

--- domkit/CssStyle.hx
+++ domkit/CssStyle.hx
@@ -130,7 +130,7 @@ class CssData {
 	function sortByPriority(r1:Rule, r2:Rule) {
 		if (r2.priority != r1.priority)
 			return r2.priority > r1.priority ? 1 : -1;
-		return r2.id > r1.id ? 1 : -1;
+		return r2.id - r1.id;
 	}
 
 	public static function registerComponent(c:Component<Dynamic,Dynamic>) {
